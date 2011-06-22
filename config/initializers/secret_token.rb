e4ce3c37d81249688f6b89f2b00898095f2d0549
Jerry Nummi <jerry@edgecase.com>
2011-06-22 09:20:25 -0400

    Base Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cascade::Application.config.secret_token = '203528136c4bc79a6ed2ec5db11f0eadd87da3691327ed62825c92c836335bdf97111534c45cac2d83d822740b0322a5e25fba3ab1097fe3947fef8c439a1a18'
